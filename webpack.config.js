9eefb538763bbc024f5acb4bf2060df8afcf8a11
jfox <fox.jonny@gmail.com>
2017-08-13 02:08:23 +0200

    adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: './runner.ts',
+    module: {
+        rules: [{
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    output: {
+        filename: './dist/bundle.js',
+        path: __dirname
+    }
+};
